03f09d54457ea97d94560eaa41ac2ac815ba99fe
cyzu <chloeyukino@gmail.com>
2017-10-16 20:52:29 +0200

    Add files via upload

---
+++ TP1 Librairies/Dynamique/dynamique.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "dynamique.h" 
+
+void test1(){
+	printf("Bienvenue dans la librairie dynamique !\n");
+}
+
+void test2(){
+	printf("Je prends moins de place qu'une librairie statique.\n");
+}
---
+++ TP1 Librairies/Dynamique/exoDynamique.c
@@ -0,0 +1,8 @@
+#include "dynamique.h"
+
+int main (void){
+	test1();
+	test2();
+	
+	return 0;
+}
